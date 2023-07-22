3a346a4c9b504c06864a515d6f79385f0d6a12ee
bharathh2004 <136560082+bharathh2004@users.noreply.github.com>
2023-07-22 09:11:52 +0530

    Add files via upload

--- class.py
+++ class.py
@@ -1,23 +1,28 @@
 class dog:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age        
+    dogname = ""
+    dogage = ""
+    dogcotcolor = ""
+    def description(self):
+        print(self.dogname)
+        print(self.dogage)
+    def get_info(self):
+        print(self.dogcotcolor)
 class JackRussellTerrier(dog):
-    def __init__(self,name,age,cot_color):
-        super().__init__(name,age) 
-        self.cot_color = cot_color
+    breed = ""
+    def breed(self):
+        print(self.breed)
 class Bulldog(JackRussellTerrier):
-    def __init__(self,name,age,cot_color,price):
-        super().__init__(name,age,cot_color)
-        self.price = price
     def fun(self):
-        print("Name of the dog is:",self.name)
-        print("Age of the dog is:",self.age )
-        print("Cot color of the dog is:",self.cot_color)
-        print("Price of the dog is:",self.price)
-obj = Bulldog('pershian',12,'RED',12000)  
-obj.fun()          
-
+        print("Name of the dog is:",self.dogname)
+        print("Age of the dog is:",self.dogage )
+        print("Cot color of the dog is:",self.dogcotcolor)
+        print("Price of the dog is:",self.dogbreed)
+obj = Bulldog()  
+obj.dogname = "bull"
+obj.dogage = 12
+obj.dogcotcolor = "red"
+obj.dogbreed = "preshian"
+obj.fun()  
 
 
 
